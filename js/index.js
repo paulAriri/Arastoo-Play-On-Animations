3b8140dc889ec6c9f9620b9f719450159407110f
paulAriri <ariripaulcoc@gmail.com>
2020-06-10 21:18:55 -0400

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openMenuBar(){
+    var element = document.getElementById("navMenu");
+    element.classList.add("active");
+}
+    
+function closeMenuBar(){
+    var element2 = document.getElementById("navMenu");
+    element2.classList.remove("active");
+}
